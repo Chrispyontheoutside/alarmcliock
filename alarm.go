8a892bf346d9ebd19c0f7a93388737759ab3e0a7
Your Name <chrispeterfrancis@gmail.com>
2020-02-24 20:41:13 -0600

    Finished main parts of the application. Fixed and included better timing process.

--- alarm.go
+++ alarm.go
@@ -3,17 +3,23 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
+	"math"
 	"os"
+	"os/exec"
+	"runtime"
 	"strconv"
 	"time"
 )
 
 func main() {
+	//My first attempt at a full golang cli application
+	//	--> Have to actualize query search into youtube
+
 	fmt.Println("Hello! Welcome to the alarmCLIock.")
 	fmt.Println("---------------------------------")
 	//Reports the current time, unformatted string -> format string coming soon
-	time := time.Now()
-	fmt.Println(time)
+	fmt.Println(time.Now())
 
 	//initializes a scanner
 	scanner := bufio.NewScanner(os.Stdin)
@@ -36,6 +42,7 @@ func main() {
 	//to confirm
 	is_righttime := "n"
 	if is_righttime != "y" {
+		//reaffirm parameters
 		fmt.Println("Are these the correct time parameters?")
 		fmt.Println("Hours: ", hours)
 		fmt.Println("Minutes: ", minutes)
@@ -63,13 +70,51 @@ func main() {
 			}
 		}
 	}
-
 	//prints hour and minutes
-	total_time := 60*hours + minutes
-	fmt.Println(total_time, " minutes total")
-
+	total_time := float64(60*hours + minutes)
+	fmt.Println(total_time, "minutes total! countdown begins now. A reminder will go out every 30 minutes if you would like to confirm")
+	start := time.Now()
+	fmt.Println("Enter the youtube search query, and the alarm clock will go to the first video!")
+	scanner.Scan()
+	query := scanner.Text()
+	pastElapsed := time.Since(start).Minutes()
+	for total_time-time.Since(start).Minutes() > 0 {
+		currentElapsed := time.Since(start).Minutes()
+		//Every five minutes, display an alert and inform user on query
+		if int(currentElapsed)%5 == 0 && math.Round(pastElapsed) != math.Round(currentElapsed) && int(currentElapsed) != 0 {
+			//reminder and report remaining time in minutes.
+			fmt.Println("It has been 5 minutes since the last alert", int(total_time)-int(currentElapsed), "minutes remain")
+			fmt.Println("Reminder that your query is: ", query)
+		}
+		pastElapsed = currentElapsed
+	}
+	//time has ended, function end
+	fmt.Println("Time has elapsed.")
+	//initialize url to pass to function
+	url := "https://www.youtube.com/results?search_query=" + query
+	openbrowser(url)
 }
 
 func errorForNow() {
 	fmt.Println("Error occurred, fix incoming, hold tight!")
+}
+
+//open browser funtion, input url
+func openbrowser(url string) {
+	var err error
+
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
